c15c14f75e8941b5e16a015cdc6061107575ff7d
krishnaveni44 <krishnaveni44@gmail.com>
2022-06-13 16:17:37 +0530

    1

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -5,7 +5,7 @@ import "./topbar.css";
 
 export default function TopBar() {
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
+  const PF = "https://blog-app-4.herokuapp.com/images/"
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -12,7 +12,7 @@ export default function Settings() {
   const [success, setSuccess] = useState(false);
 
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
+  const PF = "https://blog-app-4.herokuapp.com/images/"
 
   const handleSubmit = async (e) => {
     e.preventDefault();
