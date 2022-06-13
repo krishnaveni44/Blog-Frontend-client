909f3ec5faa4f0930121b81fd0d45e793c0f2361
krishnaveni44 <krishnaveni44@gmail.com>
2022-06-13 19:45:37 +0530

    1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="header">
       <div className="headerTitles">
         <span className="headerTitleSm">Vens</span>
-        <span className="headerTitleLg">Blog</span>
+        <span className="headerTitleLg">Story Writing App</span>
       </div>
       <img
         className="headerImg"
