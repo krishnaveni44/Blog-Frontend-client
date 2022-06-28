58c962482eb8426fdf408e84de8c39d8143adac6
krishnaveni44 <krishnaveni44@gmail.com>
2022-06-28 10:28:24 +0530

    1

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -13,7 +13,7 @@ export default function Register() {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("/auth/register", {
+      const res = await axios.post("https://blog-app-4.herokuapp.com/auth/register", {
         username,
         email,
         password,
